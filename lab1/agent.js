28d49c843750482241619f006fea4d25675ff66c
Мария Кулачкова <42891897+smotritetamptiza@users.noreply.github.com>
2022-02-20 18:36:52 +0300

    rival player placing and location

--- lab1/agent.js
+++ lab1/agent.js
@@ -2,46 +2,45 @@ const Msg = require('./msg');
 const readline = require('readline');
 
 const Flags = {
-  ftl50: {x: -50, y: 39}, ftl40: {x: -40, y: 39},
-  ftl30: {x: -30, y: 39}, ftl20: {x: -20, y: 39},
-  ftl10: {x: -10, y: 39}, ft0: {x: 0, y: 39},
-  ftr50: {x: 50, y: 39}, ftr40: {x: 40, y: 39},
-  ftr30: {x: 30, y: 39}, ftr20: {x: 20, y: 39},
-  ftr10: {x: 10, y: 39},
-  fbl50: {x: -50, y: -39}, fbl40: {x: -40, y: -39},
-  fbl30: {x: -30, y: -39}, fbl20: {x: -20, y: -39},
-  fbl10: {x: -10, y: -39}, fb0: {x: 0, y: -39},
-  fbr50: {x: 50, y: -39}, fbr40: {x: 40, y: -39},
-  fbr30: {x: 30, y: -39}, fbr20: {x: 20, y: -39},
-  fbr10: {x: 10, y: -39},
-  flt30: {x: -57.5, y: 30}, flt20: {x: -57.5, y: 20},
-  flt10: {x: -57.5, y: 10}, fl0: {x: -57.5, y: 0},
-  flb30: {x: -57.5, y: -30}, flb20: {x: -57.5, y: -20},
-  flb10: {x: -57.5, y: -10},
-  frt30: {x: 57.5, y: 30}, frt20: {x: 57.5, y: 20},
-  frt10: {x: 57.5, y: 10}, fr0: {x: 57.5, y: 0},
-  frb30: {x: 57.5, y: -30}, frb20: {x: 57.5, y: -20},
-  frb10: {x: 57.5, y: -10},
-  flt: {x: -52.5, y: 34}, flb: {x: -52.5, y: -34},
-  fglt: {x: -52.5, y: 7.01}, fglb: {x: -52.5, y: -7.01},
+  ftl50: {x: -50, y: -39}, ftl40: {x: -40, y: -39},
+  ftl30: {x: -30, y: -39}, ftl20: {x: -20, y: -39},
+  ftl10: {x: -10, y: -39}, ft0: {x: 0, y: -39},
+  ftr50: {x: 50, y: -39}, ftr40: {x: 40, y: -39},
+  ftr30: {x: 30, y: -39}, ftr20: {x: 20, y: -39},
+  ftr10: {x: 10, y: -39},
+  fbl50: {x: -50, y: 39}, fbl40: {x: -40, y: 39},
+  fbl30: {x: -30, y: 39}, fbl20: {x: -20, y: 39},
+  fbl10: {x: -10, y: 39}, fb0: {x: 0, y: 39},
+  fbr50: {x: 50, y: 39}, fbr40: {x: 40, y: 39},
+  fbr30: {x: 30, y: 39}, fbr20: {x: 20, y: 39},
+  fbr10: {x: 10, y: 39},
+  flt30: {x: -57.5, y: -30}, flt20: {x: -57.5, y: -20},
+  flt10: {x: -57.5, y: -10}, fl0: {x: -57.5, y: -0},
+  flb30: {x: -57.5, y: 30}, flb20: {x: -57.5, y: 20},
+  flb10: {x: -57.5, y: 10},
+  frt30: {x: 57.5, y: -30}, frt20: {x: 57.5, y: -20},
+  frt10: {x: 57.5, y: -10}, fr0: {x: 57.5, y: -0},
+  frb30: {x: 57.5, y: 30}, frb20: {x: 57.5, y: 20},
+  frb10: {x: 57.5, y: 10},
+  flt: {x: -52.5, y: -34}, flb: {x: -52.5, y: 34},
+  fglt: {x: -52.5, y: -7.01}, fglb: {x: -52.5, y: 7.01},
   gl: {x: -52.5, y: 0}, fplc: {x: -36, y: 0},
-  fplt: {x: -36, y: 20.15}, fplb: {x: -36, y: -20.15},
-  fc: {x: 0, y: 0}, fct: {x: 0, y: 34}, fcb: {x: 0, y: -34},
-  frt: {x: 52.5, y: 34}, frb: {x: 52.5, y: -34},
-  fgrt: {x: 52.5, y: 7.01}, fgrb: {x: 52.5, y: -7.01},
+  fplt: {x: -36, y: -20.15}, fplb: {x: -36, y: 20.15},
+  fc: {x: 0, y: 0}, fct: {x: 0, y: -34}, fcb: {x: 0, y: 34},
+  frt: {x: 52.5, y: -34}, frb: {x: 52.5, y: 34},
+  fgrt: {x: 52.5, y: -7.01}, fgrb: {x: 52.5, y: 7.01},
   gr: {x: 52.5, y: 0}, fprc: {x: 36, y: 0},
-  fprt: {x: 36, y: 20.15}, fprb: {x: 36, y: -20.15},
-  distance(p1, p2) {
-    return Math.sqrt((p1.x - p2.x)**2 + (p1.y - p2.y)**2);
-  }
+  fprt: {x: 36, y: -20.15}, fprb: {x: 36, y: 20.15}
 };
 
 class Agent {
-  constructor(velocity) {
+  constructor(velocity, locate) {
     this.position = 'l';
     this.run = false;
     this.act = null;
     this.momentum = velocity * 360 / 10;
+    this.locate = locate;
+    this.coordinates;
     /*this.rl = readline.createInterface({
       input: process.stdin,
       output: process.stdout
@@ -89,49 +88,88 @@ class Agent {
                   "cmd": { "p": ["f", ".." flag name]}
                  }
         */
-        let flags = [];
-        for (let res of p) {
-          if (res.cmd && res.cmd.p && res.cmd.p.length > 0 && res.cmd.p[0] == "f") {
-            let f_name = res.cmd.p.join('');
-            try {
-              let f = {
-                x: Flags[f_name].x,
-                y: Flags[f_name].y,
-                d: res.p[0]
+        if (this.locate) {
+          let flags = [];
+          let player;
+          let playerCoordinates;
+          for (let res of p) {
+            if (res.cmd && res.cmd.p && res.cmd.p.length > 0 && res.cmd.p[0] == "f") {
+              let f_name = res.cmd.p.join('');
+              try {
+                let f = {
+                  x: Flags[f_name].x,
+                  y: Flags[f_name].y,
+                  d: res.p[0],
+                  a: res.p[1]
+                };
+                flags.push(f);
+              } catch (e) {
+                console.log(f_name);
+                console.log(e);
+              }
+            }
+            else if (res.cmd && res.cmd.p && res.cmd.p.length > 0 && res.cmd.p[0] == "p") {
+              player = {
+                d: res.p[0],
+                a: res.p[1]
               };
-              flags.push(f);
-            } catch (e) {
-              console.log(f_name);
-              console.log(e);
             }
           }
-        }
-        if (flags.length >= 3) {
-          let coordinates;
-          for (let i = 2; i < flags.length; i++) {
-            if (flags[0].x == flags[1].x) {
-              if (flags[i].x != flags[0].x) {
-                coordinates = this.threeFlagCoordinates(flags[0], flags[1], flags[i]);
-                break;
-              }
-            } else if (flags[0].y == flags[1].y) {
-              if (flags[i].y != flags[0].y) {
-                coordinates = this.threeFlagCoordinates(flags[0], flags[1], flags[i]);
-                break;
-              }
-            } else {
-              let alpha = (flags[0].y - flags[1].y) / (flags[0].x - flags[1].x);
-              let beta = flags[0].y - alpha * flags[0].x;
-              if (flags[i].y != alpha * flags[i].x + beta) {
-                coordinates = this.threeFlagCoordinates(flags[0], flags[1], flags[i]);
-                break;
+          if (flags.length >= 3) {
+            for (let i = 2; i < flags.length; i++) {
+              if (flags[0].x == flags[1].x) {
+                if (flags[i].x != flags[0].x) {
+                  this.coordinates = this.threeFlagCoordinates(flags[0], flags[1], flags[i]);
+                  break;
+                }
+              } else if (flags[0].y == flags[1].y) {
+                if (flags[i].y != flags[0].y) {
+                  this.coordinates = this.threeFlagCoordinates(flags[0], flags[1], flags[i]);
+                  break;
+                }
+              } else {
+                let alpha = (flags[0].y - flags[1].y) / (flags[0].x - flags[1].x);
+                let beta = flags[0].y - alpha * flags[0].x;
+                if (flags[i].y != alpha * flags[i].x + beta) {
+                  this.coordinates = this.threeFlagCoordinates(flags[0], flags[1], flags[i]);
+                  break;
+                }
               }
             }
-          }
-          if (!coordinates) {
-            console.log("could not calculate coordinates");
-          } else {
-            console.log(coordinates);
+            if (!this.coordinates) {
+              console.log("could not calculate coordinates");
+              return;
+            }
+            console.log("My coordinates: " + JSON.stringify(this.coordinates));
+
+            if (player) {
+              console.log(player);
+              for (let i = 1; i < flags.length; i++) {
+                if (flags[0].x == this.coordinates.x) {
+                  if (flags[i].x != flags[0].x) {
+                    playerCoordinates = this.locateObject(flags[0], flags[i], player);
+                    break;
+                  }
+                } else if (flags[0].y == this.coordinates.y) {
+                  if (flags[i].y != flags[0].y) {
+                    playerCoordinates = this.locateObject(flags[0], flags[i], player);
+                    break;
+                  }
+                } else {
+                  let alpha = (flags[0].y - this.coordinates.y) / (flags[0].x - this.coordinates.x);
+                  let beta = flags[0].y - alpha * flags[0].x;
+                  if (flags[i].y != alpha * flags[i].x + beta) {
+                    playerCoordinates = this.locateObject(flags[0], flags[i], player);
+                    break;
+                  }
+                }
+              }
+              if (!playerCoordinates) {
+                console.log("could not calculate other player's coordinates");
+              } else {
+                console.log("Rival's coordinates: " + JSON.stringify(playerCoordinates));
+              }
+            }
           }
         }
       }
@@ -170,6 +208,29 @@ class Agent {
       x = alpha1 * y + beta1;
     }
     return { x: x, y: y };
+  }
+  calculateDistance(f, a) {
+    return Math.abs(Math.sqrt(f.d**2 + a.d**2 - 2*f.d*a.d*Math.cos(Math.abs(f.a - a.a) / 180 * Math.PI)));
+  }
+  locateObject(f1, f2, object) {
+    let da1 = this.calculateDistance(f1, object);
+    let da2 = this.calculateDistance(f2, object);
+    let flag1 = {
+      x: f1.x,
+      y: f1.y,
+      d: da1
+    }
+    let flag2 = {
+      x: f2.x,
+      y: f2.y,
+      d: da2
+    }
+    let flag3 = {
+      x: this.coordinates.x,
+      y: this.coordinates.y,
+      d: object.d
+    }
+    return this.threeFlagCoordinates(flag1, flag2, flag3);
   }
 }
 module.exports = Agent;
--- lab1/app.js
+++ lab1/app.js
@@ -2,31 +2,36 @@ const Agent = require('./agent'); //importing Agent
 const readline = require('readline');
 
 const VERSION = 7; // server version
-let teamName = 'teamA';
-let myCoordinates = "-15 0";
-let velocity = "5";
-let rival = false;
-let rivalCoordinates = "15 0";
-
+let teamName, myCoordinates, velocity, rival, rivalCoordinates;
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
+function initPlayer(teamName, coordinates, velocity, printLocation) {
+  let agent = new Agent(velocity, printLocation);
+  require('./socket')(agent, teamName, VERSION); // socket configuration
+  setTimeout(function () {
+    agent.socketSend('move', coordinates); // placing player on the field
+  }, 10);
+}
+
 rl.question("Enter team name: ", function (answer) {
-  teamName = answer;
-  rl.question("Enter initial coordinates: ", function (answer) {
-    myCoordinates = answer;
+  teamName = answer || "myTeam";
+  rl.question("Enter initial coordinates (X must be negative): ", function (answer) {
+    myCoordinates = answer || "-15 0";
     rl.question("Enter velocity: ", function (answer) {
-      velocity = answer;
+      velocity = answer || "2";
       rl.question("Do you want to place a rival player? (y/n) ", function (answer) {
         if (answer.toLowerCase() == "y") {
           rival = true;
-          rl.question("Enter initial coordinates of the rival player:   ", function (answer) {
-            rivalCoordinates = answer;
+          rl.question("Enter initial coordinates of the rival player \
+          (X must be negative, Y will be mirrored): ", function (answer) {
+            rivalCoordinates = answer || "-15 0";
             rl.close();
           })
         } else {
+          rival = false;
           rl.close();
         }
       })
@@ -35,9 +40,9 @@ rl.question("Enter team name: ", function (answer) {
 })
 
 
-rl.on('close', () => {
-  console.log(teamName);
-  let agent = new Agent(velocity);
-  require('./socket')(agent, teamName, VERSION); // socket configuration
-  agent.socketSend('move', myCoordinates); // placing player on the field
+rl.on('close', async () => {
+  await initPlayer(teamName, myCoordinates, velocity, true);
+  if (rival) {
+    await initPlayer('not_' + teamName, rivalCoordinates, 0, false);
+  }
 })
