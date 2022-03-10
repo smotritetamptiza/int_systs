98cdbffcb196f83b9fd60d6d81b729c8f05d5ad4
Maria Kulachkova <42891897+smotritetamptiza@users.noreply.github.com>
2022-03-10 13:36:05 +0300

    lab 3 started

---
+++ lab3/app.js
@@ -0,0 +1,30 @@
+const Agent = require('./agent'); //importing Agent
+const readline = require('readline');
+
+const VERSION = 7; // server version
+let teamName, myCoordinates;
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+function initPlayer(teamName, coordinates) {
+  let agent = new Agent(coordinates);
+  require('./socket')(agent, teamName, VERSION); // socket configuration
+  setTimeout(function () {
+    agent.socketSend('move', coordinates); // placing player on the field
+  }, 10);
+}
+
+rl.question("Enter team name: ", function (answer) {
+  teamName = answer || "myTeam";
+  rl.question("Enter initial coordinates (X must be negative): ", function (answer) {
+    myCoordinates = answer || "-15 0";
+    rl.close();
+  })
+})
+
+
+rl.on('close', async () => {
+  await initPlayer(teamName, myCoordinates);
+})
---
+++ lab3/singlePlayerDT.js
@@ -0,0 +1,88 @@
+const FL = "flag", KI = "kick";
+const DT = {
+  state: {
+    next: 0,
+    sequence: [{act: FL, fl: "frb"}, {act: FL, fl: "gl"},
+    {act: KI, fl: "b", goal: "gr"}],
+    command: null
+  },
+  root: {
+    exec(mgr, state) {
+      state.action = state.sequence[state.next];
+      state.command = null;
+    },
+    next: "goalVisible"
+  },
+  goalVisible: {
+    condition: (mgr, state) => mgr.getVisible(state.action.fl),
+    trueCond: "rootNext",
+    falseCond: "rotate"
+  },
+  rotate: {
+    exec(mgr, state) {
+      state.command = { n: "turn", v: "90" };
+    },
+    next: "sendCommand"
+  },
+  rootNext: {
+    condition: (mgr, state) => state.action.act == FL,
+    trueCond: "flagSeek",
+    falseCond: "ballSeek"
+  },
+  flagSeek: {
+    condition: (mgr, state) => 3 > mgr.getDistance(state.action.fl),
+    trueCond: "closeFlag",
+    falseCond: "farGoal"
+  },
+  closeFlag: {
+    exec(mgr, state) {
+      state.next++;
+      state.action = state.sequence[state.next];
+    },
+    next: "goalVisible"
+  },
+  farGoal: {
+    condition: (mgr, state) => mgr.getAngle(state.action.fl) > 4,
+    trueCond: "rotateToGoal",
+    falseCond: "runToGoal"
+  },
+  rotateToGoal: {
+    exec(mgr, state) {
+      state.command = { n: "turn", v: mgr.getAngle(state.action.fl) };
+    },
+    next: "sendCommand"
+  },
+  runToGoal: {
+    exec(mgr, state) {
+      state.command = { n: "dash", v: "100" };
+    },
+    next: "sendCommand"
+  },
+  sendCommand: {
+    command: (mgr, state) => state.command
+  },
+  ballSeek: {
+    condition: (mgr, state) => 0.5 > mgr.getDistance(state.action.fl),
+    trueCond: "closeBall",
+    falseCond: "farGoal"
+  },
+  closeBall: {
+    condition: (mgr, state) => mgr.getVisible(state.action.goal),
+    trueCond: "ballGoalVisible",
+    falseCond: "ballGoalInvisible"
+  },
+  ballGoalVisible: {
+    exec(mgr, state) {
+      state.command = { n: "kick", v: "100", a: mgr.getAngle(state.action.goal) };
+    },
+    next: "sendCommand"
+  },
+  ballGoalInvisible: {
+    exec(mgr, state) {
+      state.command = { n: "kick", v: "100", a: "90" };
+    },
+    next: "sendCommand"
+  }
+};
+
+module.exports = DT;
