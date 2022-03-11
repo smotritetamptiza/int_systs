a0b2413e766f0fd06e61d0334957146324a82d59
Maria Kulachkova <42891897+smotritetamptiza@users.noreply.github.com>
2022-03-11 12:10:43 +0300

    minor changes

--- lab3/followingDT.js
+++ lab3/followingDT.js
@@ -40,39 +40,21 @@ let DT = {
     next: "sendCommand"
   },
   angleClose: {
-    condition: (mgr, state) => (Math.abs(mgr.getAngle(state.leader)) > 40 ||
-     Math.abs(mgr.getAngle(state.leader)) < 25),
+    condition: (mgr, state) => (mgr.getAngle(state.leader) > 40 ||
+    mgr.getAngle(state.leader) < 25),
     trueCond: "rotateSlightly",
     falseCond: "closeLeader"
   },
   rotateSlightly: {
     exec(mgr, state) {
-      let angle = mgr.getAngle(state.leader);
-      if (angle > 0) {
-        angle -= 30;
-      } else {
-        angle += 30;
-      }
-      state.command = { n: "turn", v: angle };
+      state.command = { n: "turn", v: mgr.getAngle(state.leader) - 30 };
     },
     next: "sendCommand"
   },
   closeLeader: {
     condition: (mgr, state) => mgr.getDistance(state.leader) < 7,
-    trueCond: "veryCloseLeader",
+    trueCond: "runVerySlow",
     falseCond: "runSlower"
-  },
-  veryCloseLeader: {
-    condition: (mgr, state) => (mgr.getDistance(state.leader) < 1 &&
-    Math.abs(mgr.getAngle(state.leader)) < 40),
-    trueCond: "rotateClose",
-    falseCond: "runVerySlow"
-  },
-  rotateClose: {
-    exec(mgr, state) {
-      state.command = { n: "turn", v: "30" };
-    },
-    next: "sendCommand"
   },
   runVerySlow: {
     exec(mgr, state) {
